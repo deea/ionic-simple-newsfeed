5c8e6af6378db82f2964ede1ee5e73e3d6066233
Andreea Capalnas <andreea.capalnas@gmail.com>
2019-11-27 16:55:22 +0000

    Created app with side menu, set up sign in page and linked it to master list on button press and updated rooting

---
+++ src/pages/list/list_test.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  images: string[];
+  items: Array<{title: string, description: string, image: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    // Let's populate this page with some filler content for funzies
+    this.images = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        description: 'This is item #' + i,
+        image: this.images[Math.floor(Math.random() * this.images.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(ListPage, {
+      item: item
+    });
+  }
+}
